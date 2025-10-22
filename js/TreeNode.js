13331f23f74770dbd9ea0eb5ab6dc332be34325f
tiago-rodrigues1 <tiagords127@gmail.com>
2025-10-22 10:47:04 -0300

    refactor: rename and format

--- js/TreeNode.js
+++ js/TreeNode.js
@@ -1,15 +1,23 @@
-class Node {
+export class TreeNode {
   constructor(value, { left = null, right = null, parent = null } = {}) {
     this.value = value;
     this.left = left;
     this.right = right;
-    this.parent = parent; 
+    this.parent = parent;
     this.height = 1;
   }
 
-  hasLeft() { return this.left !== null; }
-  hasRight() { return this.right !== null; }
-  isLeaf() { return !this.left && !this.right; }
+  hasLeft() {
+    return this.left !== null;
+  }
+
+  hasRight() {
+    return this.right !== null;
+  }
+  
+  isLeaf() {
+    return !this.left && !this.right;
+  }
 
   setLeft(node) {
     this.left = node;
